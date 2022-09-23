88e6ddecbcd5bcdbfbb7db853a9f9da74aa05fa3
Gyumin Sim <gyumin.sim@privatenote.co.kr>
2022-09-24 01:29:46 +0900

    html, css 캐시 방지

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ window.PNOTE_BASE_URL = window.PNOTE_BASE_URL || '/';
 
 const getBodyHtml = async () => {
   const baseUrl = window.PNOTE_BASE_URL;
-  const res = await fetch(`${baseUrl}body.html`);
+  const res = await fetch(`${baseUrl}body.html?t=${Date.now()}`);
   return await res.text();
 };
 
@@ -11,6 +11,13 @@ const injectBodyHtml = async () => {
   body.insertAdjacentHTML('afterbegin', await getBodyHtml());
 }
 
+const injectStyleCss = () => {
+  const baseUrl = window.PNOTE_BASE_URL;
+  const head = document.querySelector('head');
+  head.insertAdjacentHTML('beforeend', `<link href="${baseUrl}/style.css?t=${Date.now()}" rel="stylesheet">`);
+}
+
 window.onload = async () => {
+  injectStyleCss();
   await injectBodyHtml();
 };
