be837cbcf0f40a041a7e95922dc27fcedcba2dcc
Soohyeon Lee <soohyeon.lee@privatenote.co.kr>
2022-12-01 19:25:44 +0900

    채널톡 대체 스크립트 main.js에 삽입

--- main.js
+++ main.js
@@ -29,7 +29,42 @@ const injectStyleCss = () => {
   head.insertAdjacentHTML('beforeend', `<link href="${baseUrl}/style.css?t=${Date.now()}" rel="stylesheet">`);
 }
 
+const channelScriptInit = async () => {
+  var w = window;
+  if (w.ChannelIOWrapper) {
+    return (window.console.error || window.console.log || function(){})('ChannelIO script included twice.');
+  }
+  if (w.ChannelIOInitialized) {
+    return;
+  }
+  w.ChannelIOInitialized = true;
+  var s = document.createElement('script');
+  s.type = 'text/javascript';
+  s.async = true;
+  s.src = 'https://pnote-channel-io-script.s3.ap-northeast-2.amazonaws.com/channel-io-script-min.js';
+  s.setAttribute('channel-btn-img', 'https://pnote.s3.ap-northeast-2.amazonaws.com/channel-images/channel-button.png')
+  s.setAttribute('channel-btn-img-m', 'https://pnote.s3.ap-northeast-2.amazonaws.com/channel-images/channel-button-m.png')
+  s.charset = 'UTF-8';
+  var x = document.getElementsByTagName('script')[0];
+  x.parentNode && x.parentNode.insertBefore(s, x);
+}
+
+const channelScriptBoot = async () => {
+  if (window.ChannelIOWrapper && !window.ChannelIOWrapper.q) {
+    window.ChannelIOWrapper('boot', {
+      pluginKey: "204ad750-a3ee-4182-a8c5-05883589bd0c"
+    });
+    window.ChannelIOWrapper('showChannelButton');
+  } else {
+    setTimeout(async () => {
+      await channelScriptBoot();
+    }, 100);
+  }
+}
+
 window.onload = async () => {
   injectStyleCss();
   await injectBodyHtml();
+  await channelScriptInit();
+  await channelScriptBoot();
 };
